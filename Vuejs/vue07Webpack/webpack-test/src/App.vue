a98dfa26bef5829ef1d09f64f06a07b06d69b974
faeqf <qefqfqff@naver.com>
2020-06-28 18:10:34 +0900

    200628_13-10숙제

--- Vuejs/vue07Webpack/webpack-test/src/App.vue
+++ Vuejs/vue07Webpack/webpack-test/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <header-comp></header-comp>
-    
+
     <div class="row">
       <!-- 여기 컨텐츠 -->
       <section id="page1" data-role="page">
@@ -20,12 +20,12 @@ import Footer from "./Footer.vue";
 export default {
   data() {
     return {
-      msg: "Hello VueJS"
+      msg: "hello, vuejs",
     };
   },
   components: {
     "header-comp": Header,
-    "footer-comp": Footer
-  }
+    "footer-comp": Footer,
+  },
 };
 </script>
